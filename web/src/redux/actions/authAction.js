bfbd4dc34d5f75eade9cff5a5f510446f8041f9c
TranAnhTu17 <20020495@vnu.edu.vn>
2022-11-21 15:46:51 +0700

    feat(web): add login page

---
+++ web/src/redux/actions/authAction.js
@@ -0,0 +1,73 @@
+import { AUTH } from "../types";
+import { postDataAPI } from "../../utils/fetchData";
+
+export const login = (data) => async (dispatch) => {
+  try {
+    // dispatch({ type: ALERT, payload: { loading: true } });
+    const res = await postDataAPI("auth/login", data);
+    dispatch({
+      type: AUTH,
+      payload: {
+        token: res.data.access_token,
+        user: res.data.user,
+      },
+    });
+
+    localStorage.setItem("loggedIn", true);
+    // dispatch({
+    //   type: ALERT,
+    //   payload: {
+    //     success: res.message,
+    //   },
+    // });
+  } catch (err) {
+    // dispatch({
+    //   type: ALERT,
+    //   payload: {
+    //     error: err.response.data.message,
+    //   },
+    // });
+  }
+};
+
+export const refreshToken = () => async (dispatch) => {
+  const loggedIn = localStorage.getItem("loggedIn");
+  if (loggedIn) {
+    // dispatch({ type: ALERT, payload: { loading: true } });
+
+    try {
+      const res = await postDataAPI("auth/refresh_token");
+      dispatch({
+        type: AUTH,
+        payload: {
+          token: res.data.access_token,
+          user: res.data.user,
+        },
+      });
+
+      // dispatch({ type: ALERT, payload: {} });
+    } catch (err) {
+      // dispatch({
+      //   type: ALERT,
+      //   payload: {
+      //     error: err.response.data.message,
+      //   },
+      // });
+    }
+  }
+};
+
+export const logout = () => async (dispatch) => {
+  try {
+    localStorage.removeItem("loggedIn");
+    await postDataAPI("auth/logout");
+    window.location.href = "/";
+  } catch (err) {
+    // dispatch({
+    //   type: ALERT,
+    //   payload: {
+    //     error: err.response.data.message,
+    //   },
+    // });
+  }
+};
